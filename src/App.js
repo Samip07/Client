1a35469da02cda2a469b9b6bc37b11c7e5fbd254
Samip07 <samippoudel40@gmail.com>
2024-03-31 12:44:17 +0545

    Prepare build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Home, Visitings, About, Stay, Register, Error, Trek } from "./pages";
 
 function App() {
